cdb19c19b801507a11ae8117a317eb76d3fb4414
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-18 02:35:07 +0530

    update program.cs

--- Source-Code/Practical-sheet-2-T1/program.cs
+++ Source-Code/Practical-sheet-2-T1/program.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace KiloMeter_to_Meter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
