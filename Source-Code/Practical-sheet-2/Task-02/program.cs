0bd6ae6954f1af52f466acf409879abfc829f77c
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-18 03:12:21 +0530

    upate file

--- Source-Code/Practical-sheet-2/Task-02/program.cs
+++ Source-Code/Practical-sheet-2/Task-02/program.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiloMeter_to_Meter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometer;
+
+           
+            Console.ReadKey();
+        }
+    }
+}
 
