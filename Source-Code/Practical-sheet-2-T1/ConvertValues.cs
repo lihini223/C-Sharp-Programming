93aa94483ccd9a328e113045af9b2c106761c65e
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-18 02:39:01 +0530

    create method kilometer to meter

--- Source-Code/Practical-sheet-2-T1/ConvertValues.cs
+++ Source-Code/Practical-sheet-2-T1/ConvertValues.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiloMeter_to_Meter
+{
+    class ConvertValues
+    {
+        public void KilometerToMeter()
+        {
+            double kilometer,meter;
+
+
+        }
+    }
+}
 
