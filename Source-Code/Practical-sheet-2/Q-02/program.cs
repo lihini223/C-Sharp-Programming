6563cf959e7e7948160a768408280207fe72ca89
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 05:52:34 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-02/program.cs
+++ Source-Code/Practical-sheet-2/Q-02/program.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius,A,C;
+
+         
+        }
+    }
+}
 
