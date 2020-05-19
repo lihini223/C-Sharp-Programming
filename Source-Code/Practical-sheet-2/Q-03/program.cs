ceac068d9c41ce49253d76229ab3893a0c1dcf14
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-20 03:14:07 +0530

    updatae file

--- Source-Code/Practical-sheet-2/Q-03/program.cs
+++ Source-Code/Practical-sheet-2/Q-03/program.cs
@@ -1,1 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, ans=0;
+            int index;
+            
+            Console.WriteLine("Welcome To My Calculator !!!");
+            Console.WriteLine();
+            Console.WriteLine("Enter 1 for Addition");
+            Console.WriteLine("Enter 2 for Substraction");
+            Console.WriteLine("Enter 3 for Multiplication");
+            Console.WriteLine("Enter 4 for Division");
+
+            
+            Console.WriteLine("Enter Your Choice:");
+            index = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter first number:");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first number:");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            CalculateValues values = new CalculateValues();
+
+            switch (index)
+            {
+                case 1:
+                    ans = values.Addition(n1,n2);
+                    break;
+                case 2:
+                    ans = values.Substraction(n1,n2);
+                    break;
+                case 3:
+                    ans = values.Multiplication(n1,n2);
+                    break;
+                case 4:
+                    ans = values.Division(n1,n2);
+                    break;
+            }
+
+            Console.WriteLine("Your Answer is : {0}", ans);
+
+            Console.ReadKey();
+
+        }
+    }
+}
