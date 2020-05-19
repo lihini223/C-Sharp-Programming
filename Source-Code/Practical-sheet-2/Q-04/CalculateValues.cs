6820c30146c5ffb0a736aa818620bc11bb0bd7b9
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 07:44:29 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-04/CalculateValues.cs
+++ Source-Code/Practical-sheet-2/Q-04/CalculateValues.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculateValues
+    {
+        public double Addition(double n1, double n2)
+        {
+            double total;
+            total = n1 + n2;
+            return total; 
+        }
+
+ 
+    }
+}
 
