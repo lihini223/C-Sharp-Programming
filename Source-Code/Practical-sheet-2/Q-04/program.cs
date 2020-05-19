8d9145f04621769db054691370eccb6ad2afcea9
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 07:39:38 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-04/program.cs
+++ Source-Code/Practical-sheet-2/Q-04/program.cs
@@ -13,7 +13,13 @@ namespace Calculator
         {
             double n1, n2, ans=0;
             int index;
-
+            
+            Console.WriteLine("Welcome To My Calculator !!!");
+            Console.WriteLine();
+            Console.WriteLine("Enter 1 for Addition");
+            Console.WriteLine("Enter 2 for Substraction");
+            Console.WriteLine("Enter 3 for Multiplication");
+            Console.WriteLine("Enter 4 for Division");
 
         }
     }
