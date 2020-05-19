16b44649a9e3682e8a17f974b4f5c93bee20132a
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 07:30:46 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-01/Task-02/ConvertValues.cs
+++ Source-Code/Practical-sheet-2/Q-01/Task-02/ConvertValues.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiloMeter_to_Meter
+{
+    class ConvertValues
+    {
+        public void KilometerToMeter(double km)
+        {
+            double meter;
+
+            meter = km * 1000;
+
+            Console.WriteLine("{0} km = {1} m", km, meter);
+
+        }
+    }
+}
 
