7ba7b78cb3b879467f41892e3fcbe64282ec5dcf
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 07:38:54 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-04/program.cs
+++ Source-Code/Practical-sheet-2/Q-04/program.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, ans=0;
+            int index;
+
+
+        }
+    }
+}
 
