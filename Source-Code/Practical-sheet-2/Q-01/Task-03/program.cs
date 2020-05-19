42afa905d0a2115408f8a40be101893b3077c8a5
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-19 07:33:14 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-01/Task-03/program.cs
+++ Source-Code/Practical-sheet-2/Q-01/Task-03/program.cs
@@ -1,1 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiloMeter_to_Meter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometer,meter;
+
+            Console.WriteLine("Convert Kilometers to meters");
+            
+            Console.Write("Enter Kilometers to convert:");
+            kilometer = Convert.ToDouble(Console.ReadLine());
+            
+            ConvertValues convertKM = new ConvertValues();
+            meter = convertKM.KilometerToMeter(kilometer);
+
+            Console.WriteLine("{0} km = {1} m", kilometer,meter);
+
+
+            Console.ReadKey();
+        }
+    }
+}
 
