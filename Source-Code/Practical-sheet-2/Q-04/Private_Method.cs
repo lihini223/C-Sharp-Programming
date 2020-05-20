6a7f7ddeca6752f6a22e82a3e788b6a9721d8ff1
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-20 09:29:57 +0530

    update file

---
+++ Source-Code/Practical-sheet-2/Q-04/Private_Method.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Method
+{
+    class PrivateMethod
+    {
+        private void SayHello()
+        {
+            Console.WriteLine("Hello !!");
+        }
+    }
+}
+
+
+
+
