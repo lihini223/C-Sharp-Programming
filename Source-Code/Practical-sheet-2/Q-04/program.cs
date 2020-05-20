a82a7a15ebf39ecbf385a28489d27bda7f4584c7
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-20 09:28:27 +0530

    update file

--- Source-Code/Practical-sheet-2/Q-04/program.cs
+++ Source-Code/Practical-sheet-2/Q-04/program.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateMethod hello = new PrivateMethod();
+            hello.SayHello();
+        }
+    }
+}
 
