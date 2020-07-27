ec70ea4a622186201d528acbb0c08c13f497d4ef
Lihini Nisansala <lihininisansala95@gmail.com>
2020-07-27 22:51:05 +0530

    add project files

---
+++ ADO.net/SignUp Form Project/SignUpForm/Home.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignUpForm
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            lblwelcome.BackColor = Color.Transparent;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signup_Click(object sender, EventArgs e)
+        {
+            SignUp signup = new SignUp();
+            signup.Show();
+            this.Hide();
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            Delete dl = new Delete();
+            dl.Show();
+            this.Hide();
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            Update up = new Update();
+            up.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ ADO.net/SignUp Form Project/SignUpForm/Update.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SignUpForm
+{
+    public partial class Update : Form
+    {
+        public Update()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\C# Projects\UserDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+            string Update = "UPDATE SignUp SET Password = '"+password+"' WHERE UserName = '"+username+"'";
+            SqlCommand cmd = new SqlCommand(Update,con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Updated Successfully!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+            this.Hide();
+        }
+    }
+}
