ef9d0e50e272ed445d64ce258fb101765451682d
Lihini Nisansala <lihininisansala95@gmail.com>
2020-08-03 03:54:24 +0530

    show data.designer.cs

---
+++ CRUD-APP/CRUD_APP/ShowData.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CRUD_APP
+{
+    partial class ShowData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserdataGridView = new System.Windows.Forms.DataGridView();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UserdataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UserdataGridView
+            // 
+            this.UserdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UserdataGridView.Location = new System.Drawing.Point(57, 73);
+            this.UserdataGridView.Name = "UserdataGridView";
+            this.UserdataGridView.Size = new System.Drawing.Size(475, 240);
+            this.UserdataGridView.TabIndex = 0;
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowData.Location = new System.Drawing.Point(57, 328);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(179, 67);
+            this.btnShowData.TabIndex = 1;
+            this.btnShowData.Text = "Show Data";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            this.btnShowData.Click += new System.EventHandler(this.btnShowData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Show Data";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(308, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 67);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ShowData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 398);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.UserdataGridView);
+            this.Name = "ShowData";
+            this.Text = "ShowData";
+            ((System.ComponentModel.ISupportInitialize)(this.UserdataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView UserdataGridView;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
