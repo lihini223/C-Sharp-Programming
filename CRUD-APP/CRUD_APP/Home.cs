1fa9f941bce5fa8fe3830aa1099c57b5349fc459
Lihini Nisansala <lihininisansala95@gmail.com>
2020-08-03 03:52:15 +0530

    add home.cs

---
+++ CRUD-APP/CRUD_APP/Home.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_APP
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SignUp sign_up = new SignUp();
+            sign_up.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowData show = new ShowData();
+            show.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Delete delete = new Delete();
+            delete.Show();
+            this.Show();
+
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            Update update = new Update();
+            update.Show();
+            this.Hide();
+        }
+    }
+}
